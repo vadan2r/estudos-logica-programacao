a135c3ca01f6f9f2a8b8d0ecf41ae8ae6a2aa53c
vadan2r <vadan.2r@gmail.com>
2025-07-12 20:43:27 -0300

    Projeto DIO Utilizando Recursos do Github em um Projeto Open Source

---
+++ Python/ex01.py
@@ -0,0 +1,7 @@
+# Criar uma variavel tipo string e uma lista que contenha varias strings
+# Exibir o nome da lista e os itens da lista
+frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"]
+nome = "Lista de Frutas"
+print(nome)
+for fruta in frutas:
+    print(fruta)
